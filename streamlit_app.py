eb7a9935a594aecc78ecfd9d02e89cea97ee6121
yewoneeed <20242621@sdhs.gwe.hs.kr>
2024-11-18 10:28:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import gdown
 
 # Google Drive 파일 ID
-file_id = '1NKIhMhUeRC0vPptHwT4it-LMYhamVDyi'
+file_id = '1-03foX8RcrtHWAEBzlsGFVLFXDu2ETF6'
 
 # Google Drive에서 파일 다운로드 함수
 @st.cache(allow_output_mutation=True)
